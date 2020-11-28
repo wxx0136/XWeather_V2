4304221fd7f0787938d44994b7046f9ad0b9d6da
wxx <wxx0136@gmail.com>
2020-11-27 23:19:11 -0600

    Main activity UI modified.

--- app/src/main/java/com/example/xweather_v2/CityWeatherFragment.java
+++ app/src/main/java/com/example/xweather_v2/CityWeatherFragment.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class CityWeatherFragment extends Fragment {
+
+    ImageView img_weather;
+    TextView txt_city_name, txt_temperature, txt_description, txt_date_time, txt_maxTemp, txt_minTemp,
+            txt_sunrise, txt_sunset, txt_chanceOfSnow, txt_wind, txt_feelsLike, txt_precipitation, txt_pressure, txt_humidity, txt_visibility, txt_uvIndex;
+    LinearLayout panel_today_general, panel_today_details;
 
     public CityWeatherFragment() {
         // Required empty public constructor
@@ -17,6 +25,30 @@ public class CityWeatherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_city_weather, container, false);
+        View view = inflater.inflate(R.layout.fragment_city_weather, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View itemView) {
+//        panel_today_general
+        img_weather = (ImageView) itemView.findViewById(R.id.img_weather);
+        txt_city_name = (TextView) itemView.findViewById(R.id.txt_city_name);
+        txt_temperature = (TextView) itemView.findViewById(R.id.txt_temperature);
+        txt_description = (TextView) itemView.findViewById(R.id.txt_description);
+        txt_date_time = (TextView) itemView.findViewById(R.id.txt_date_time);
+        txt_maxTemp = (TextView) itemView.findViewById(R.id.txt_maxTemp);
+        txt_minTemp = (TextView) itemView.findViewById(R.id.txt_minTemp);
+//        panel_today_details
+        txt_sunrise = (TextView) itemView.findViewById(R.id.txt_sunrise);
+        txt_sunset = (TextView) itemView.findViewById(R.id.txt_sunset);
+        txt_chanceOfSnow = (TextView) itemView.findViewById(R.id.txt_chanceOfSnow);
+        txt_wind = (TextView) itemView.findViewById(R.id.txt_wind);
+        txt_feelsLike = (TextView) itemView.findViewById(R.id.txt_feelsLike);
+        txt_precipitation = (TextView) itemView.findViewById(R.id.txt_precipitation);
+        txt_pressure = (TextView) itemView.findViewById(R.id.txt_pressure);
+        txt_humidity = (TextView) itemView.findViewById(R.id.txt_humidity);
+        txt_visibility = (TextView) itemView.findViewById(R.id.txt_visibility);
+        txt_uvIndex = (TextView) itemView.findViewById(R.id.txt_uvIndex);
     }
 }
--- app/src/main/java/com/example/xweather_v2/MainActivity.java
+++ app/src/main/java/com/example/xweather_v2/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.xweather_v2;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView main_img_add_city, main_img_more;
+    LinearLayout main_layout_point;
+    ViewPager main_vp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main_img_add_city = findViewById(R.id.main_img_add_city);
+        main_img_more = findViewById(R.id.main_img_more);
+        main_layout_point = findViewById(R.id.main_layout_point);
+        main_vp = findViewById(R.id.main_vp);
+
+//        添加点击事件
+        main_img_add_city.setOnClickListener(this);
+        main_img_more.setOnClickListener(this);
+
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        switch (view.getId()) {
+            case R.id.main_img_add_city:
+//                intent.setClass(this,CityManagerActivity.class);
+                break;
+            case R.id.main_img_more:
+//                intent.setClass(this,MoreActivity.class);
+                break;
+        }
+        startActivity(intent);
     }
 }
