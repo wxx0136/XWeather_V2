fcd209df3915c31129594a5a7b21a3b89aea03dd
wxx <wxx0136@gmail.com>
2020-12-04 02:20:23 -0600

    Finish hourly forecast recycle view.

--- app/src/main/java/com/example/xweather_v2/today_weather/HourlyForecastAdapter.java
+++ app/src/main/java/com/example/xweather_v2/today_weather/HourlyForecastAdapter.java
@@ -37,6 +37,7 @@ public class HourlyForecastAdapter extends RecyclerView.Adapter<HourlyForecastAd
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         String hour = Common.convertUnixToHour(oneCallBean.getHourly().get(position + 1).getDt()).split(":")[0];
+        double pop = oneCallBean.getHourly().get(position + 1).getPop() * 100;
 
         String unit_temp;
         if (Common.units.equals("metric")) {
@@ -45,8 +46,11 @@ public class HourlyForecastAdapter extends RecyclerView.Adapter<HourlyForecastAd
             unit_temp = "℉";
         }
 
-        holder.txt_listView_hourly_item_clock.setText(hour);
-        holder.txt_listView_hourly_item_temperature.setText(Math.round(oneCallBean.getHourly().get(position + 1).getTemp()) + unit_temp);
+        holder.txt_recycler_hourly_item_clock.setText(hour);
+        holder.txt_recycler_hourly_item_temperature.setText(Math.round(oneCallBean.getHourly().get(position + 1).getTemp()) + unit_temp);
+        if (pop != 0) {
+            holder.txt_recycler_hourly_item_pop.setText(Double.valueOf(pop).intValue() + "%");
+        }
 
         String iconURL = "https://openweathermap.org/img/wn/" + oneCallBean.getHourly().get(position + 1).getWeather().get(0).getIcon() + "@2x.png";
         Picasso.get().load(iconURL).into(holder.image_listView_hourly_item_weather);
@@ -58,15 +62,16 @@ public class HourlyForecastAdapter extends RecyclerView.Adapter<HourlyForecastAd
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView txt_listView_hourly_item_clock, txt_listView_hourly_item_temperature;
+        TextView txt_recycler_hourly_item_clock, txt_recycler_hourly_item_temperature, txt_recycler_hourly_item_pop;
         ImageView image_listView_hourly_item_weather;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            txt_listView_hourly_item_clock = itemView.findViewById(R.id.txt_listView_hourly_item_clock);
-            txt_listView_hourly_item_temperature = itemView.findViewById(R.id.txt_listView_hourly_item_temperature);
+            txt_recycler_hourly_item_clock = itemView.findViewById(R.id.txt_recycler_hourly_item_clock);
+            txt_recycler_hourly_item_temperature = itemView.findViewById(R.id.txt_recycler_hourly_item_temperature);
+            txt_recycler_hourly_item_pop = itemView.findViewById(R.id.txt_recycler_hourly_item_pop);
 
-            image_listView_hourly_item_weather = itemView.findViewById(R.id.image_listView_hourly_item_weather);
+            image_listView_hourly_item_weather = itemView.findViewById(R.id.image_recycler_hourly_item_weather);
         }
     }
 }
